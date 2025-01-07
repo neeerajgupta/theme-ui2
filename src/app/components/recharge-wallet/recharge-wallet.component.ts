60ed187d88ff2c5516965237f98fea258c1b2178
Neeraj Gupta <soft.guptaneeraj@gmail.com>
2025-01-07 21:35:59 +0530

    neww things added ajit

--- src/app/components/recharge-wallet/recharge-wallet.component.ts
+++ src/app/components/recharge-wallet/recharge-wallet.component.ts
@@ -3,20 +3,25 @@ import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { TableModule } from 'primeng/table';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-recharge-wallet',
   standalone: true,
-  imports: [CardModule,ButtonModule,InputTextModule,TableModule],
+  imports: [CardModule,ButtonModule,InputTextModule,TableModule,FormsModule],
   templateUrl: './recharge-wallet.component.html',
   styleUrl: './recharge-wallet.component.scss'
 })
 export class RechargeWalletComponent implements OnInit{
- 
+  value:number=0;
   
   products:any;
   ngOnInit(): void {
   
+  }
+  addValue(values: number) {
+    this.value = values; 
+    console.log('Current value:', this.value); 
   }
 
 }
